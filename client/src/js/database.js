65a5c8adf4459183f7a4275e3dc10599198c2a6f
Ronasian <119774818+Ronasian@users.noreply.github.com>
2023-06-04 08:10:30 -0700

    edited comments for methods

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,7 +12,7 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
+// Method that accepts some content and adds it to the database
 export const putDb = async (content) => {
   console.log('PUT request to database');
   const jateDb = await openDB('jate', 1);
@@ -23,7 +23,7 @@ export const putDb = async (content) => {
   console.log('Data saved!', result.value);
 };
 
-// TODO: Add logic for a method that gets all the content from the database
+// Method that gets all the content from the database
 export const getDb = async () => {
   console.log('GET from database');
   const jateDb = await openDB('jate', 1);
@@ -34,7 +34,6 @@ export const getDb = async () => {
   result
     ? console.log('Data retrieved!', result.value)
     : console.log('Data not found.');
-  return result?.value;
 };
 
 initdb();
